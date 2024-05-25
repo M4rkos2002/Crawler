d3110b57120ddba2626136280e8db52d7458c564
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-05-25 11:09:44 -0300

    entities persistence, modify id

--- src/main/kotlin/sirius/challenge/crawler/model/Word.kt
+++ src/main/kotlin/sirius/challenge/crawler/model/Word.kt
@@ -4,7 +4,7 @@ import jakarta.persistence.*
 
 @Entity
 class Word(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long? = null,
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long = 0,
     @Column(nullable = false) var content: String = "",
     @OneToOne(mappedBy = "word", fetch = FetchType.EAGER)  var frequency: Frequency? = null,
 )
